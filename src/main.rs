d60aac4a49e722d80fa306c80537d54d3def3b91
Timo Prüße <chroma91@gmail.com>
2022-03-26 20:41:28 +0100

    add CLI interface

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,42 @@ pub mod config;
 pub mod task_runner;
 pub mod utils;
 
-use std::env;
+use clap::{Parser, Subcommand};
+use ergo_fs::{expand, PathBuf};
+
+use crate::task_runner::TaskRunnerMode;
+
+/// Machine Setup CLI
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Args {
+    /// what should be done?
+    #[clap(subcommand)]
+    mode: Option<TaskRunnerMode>,
+
+    /// path to the config file
+    #[clap(short, long, default_value = "./machine_setup.yaml")]
+    #[clap(global = true)]
+    file: String,
+}
 
 fn main() {
-    let config_path = format!(
-        "{}/{}",
-        env::current_dir().unwrap().to_str().unwrap(),
-        "test.yml"
-    );
+    let args = Args::parse();
 
-    let run = task_runner::install(&config_path);
+    let config_path = expand(&args.file);
+    if config_path.is_err() {
+        eprintln!("{}", config_path.err().unwrap());
+        std::process::exit(1);
+    }
+
+    let mode = &args.mode.unwrap_or(TaskRunnerMode::Install);
+
+    let run = task_runner::run(&config_path.unwrap(), TaskRunnerMode::Install);
 
     if run.is_err() {
-        println!("{}", run.unwrap_err());
+        eprintln!("{}", run.unwrap_err());
+        std::process::exit(1);
     }
 
     println!("... DONE ...");
--- src/task_runner.rs
+++ src/task_runner.rs
@@ -1,3 +1,6 @@
+use clap::Subcommand;
+use core::fmt;
+use std::str::FromStr;
 use yaml_rust::yaml::Hash;
 
 use crate::{
@@ -5,10 +8,35 @@ use crate::{
     config::{base_config::BaseConfig, yaml_config::YamlConfig},
 };
 
-enum TaskRunnerMode {
+#[derive(Subcommand, Debug)]
+pub enum TaskRunnerMode {
+    /// Install all of the defined tasks
     Install,
+
+    /// Update all of the defined tasks
     Update,
+
+    /// Uninstall all of the defined tasks
     Uninstall,
+}
+
+impl fmt::Display for TaskRunnerMode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+impl FromStr for TaskRunnerMode {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "install" => Ok(TaskRunnerMode::Install),
+            "update" => Ok(TaskRunnerMode::Update),
+            "uninstall" => Ok(TaskRunnerMode::Uninstall),
+            _ => Err(format!("Invalid mode: {}", s)),
+        }
+    }
 }
 
 fn get_config_handler(config_path: &str) -> Result<Box<dyn BaseConfig>, String> {
@@ -16,7 +44,7 @@ fn get_config_handler(config_path: &str) -> Result<Box<dyn BaseConfig>, String> 
 
     return match file_ending {
         file_ending if file_ending == "yml" || file_ending == "yaml" => Ok(Box::new(YamlConfig {})),
-        _ => Err(String::from("Unsupported config file format")),
+        _ => Err(format!("Unsupported config file format: .{}", file_ending)),
     };
 }
 
@@ -32,7 +60,7 @@ fn run_command(
     };
 }
 
-fn run(config_path: &str, mode: TaskRunnerMode) -> Result<(), String> {
+pub fn run(config_path: &str, mode: TaskRunnerMode) -> Result<(), String> {
     let config = get_config_handler(config_path);
     if config.is_err() {
         return Err(config.err().unwrap());
@@ -45,6 +73,12 @@ fn run(config_path: &str, mode: TaskRunnerMode) -> Result<(), String> {
         return Err(result.err().unwrap());
     }
 
+    match mode {
+        TaskRunnerMode::Install => println!("Installing..."),
+        TaskRunnerMode::Update => println!("Updating..."),
+        TaskRunnerMode::Uninstall => println!("Uninstalling..."),
+    }
+
     for task in result.unwrap().tasks {
         println!("Running task \"{}\" ...", task.name);
 
@@ -54,8 +88,8 @@ fn run(config_path: &str, mode: TaskRunnerMode) -> Result<(), String> {
             let result = run_command(resolved_command, command.args, &mode);
 
             if result.is_err() {
-                println!("ERR: {}", command.name);
-                println!("{:?}", result.unwrap_err());
+                eprintln!("ERR: {}", command.name);
+                eprintln!("{:?}", result.unwrap_err());
             } else {
                 println!("OK: {}", command.name);
             }
@@ -63,22 +97,4 @@ fn run(config_path: &str, mode: TaskRunnerMode) -> Result<(), String> {
     }
 
     return Ok(());
-}
-
-pub fn install(config_path: &str) -> Result<(), String> {
-    println!("Installing ...");
-
-    return run(config_path, TaskRunnerMode::Install);
-}
-
-pub fn update(config_path: &str) -> Result<(), String> {
-    println!("Updating ...");
-
-    return run(config_path, TaskRunnerMode::Update);
-}
-
-pub fn uninstall(config_path: &str) -> Result<(), String> {
-    println!("Uninstalling ...");
-
-    return run(config_path, TaskRunnerMode::Uninstall);
 }
